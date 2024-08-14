eafe76cff7ff67a77ff5ba958c7f13d5fda1ef96
Namgyeon <getam101@naver.com>
2024-08-14 22:39:02 +0900

    Add to Cart버튼 클릭시 Your Cart로 상품 옮기는 함수(중복제거 기능 추가)구현

--- product-list-with-cart-main/index.js
+++ product-list-with-cart-main/index.js
@@ -1,9 +1,3 @@
-// 수량 버튼 변수
-const minusButtons = document.querySelectorAll(".minus");
-const plusButtons = document.querySelectorAll(".plus");
-// Add to Cart 이벤트 변수
-const addCartBtns = document.querySelectorAll(".addCart");
-
 // minusButtons을 클릭하면 numElement.textContent의 값이 -1된다.
 function handleMinusClick(event) {
   const quantity = event.target.closest(".quantitySelector");
@@ -22,6 +16,10 @@ function handlePlusClick(event) {
   numElement.textContent = num + 1;
 }
 
+// 수량 버튼 변수
+const minusButtons = document.querySelectorAll(".minus");
+const plusButtons = document.querySelectorAll(".plus");
+
 ///이벤트
 
 minusButtons.forEach((button) => {
@@ -32,11 +30,64 @@ plusButtons.forEach((button) => {
   button.addEventListener("click", handlePlusClick);
 });
 
+// Add to Cart 이벤트 변수
+const addCartBtns = document.querySelectorAll(".addCart");
+
 // Add to Cart 클릭시 버튼 전환
 addCartBtns.forEach((addCartBtn) => {
   addCartBtn.addEventListener("click", (event) => {
     const quantitySelector =
       event.currentTarget.querySelector(".quantitySelector");
     quantitySelector.style.display = "flex";
+    // orderContainer 안의 요소들 없애기.
+    orderContainer.style.display = "none";
+  });
+});
+
+// container2 orderContainer
+const orderContainer = document.querySelector(".orderContainer");
+const orderContainer2 = document.querySelector(".orderContainer2");
+
+// Add to Cart 버튼눌렀을때 Your Cart로 상품 옮기는 함수
+addCartBtns.forEach((addCartBtn) => {
+  addCartBtn.addEventListener("click", (event) => {
+    // orderContainer 요소 생성(이름, 수량, 가격)
+    const orderName = event.target.closest(".item").querySelector(".name");
+    let quantity = event.target.closest(".item").querySelector(".num");
+    const price = event.target.closest(".item").querySelector(".price");
+    // div태그
+    const div1 = document.createElement("div");
+    const div2 = document.createElement("div");
+    const div3 = document.createElement("div");
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    const p3 = document.createElement("p");
+    div1.setAttribute("class", "itemBox1");
+    div2.setAttribute("class", "itemBox2");
+    div3.setAttribute("class", "itemBox3");
+    p1.setAttribute("class", "itemName");
+    p2.setAttribute("class", "itemNum");
+    p3.setAttribute("class", "itemPrice");
+
+    // orderContainer2에 이미 orderName이 존재하면 작동안한다.
+    const orderNames = orderContainer2.querySelectorAll(".itemBox1");
+    let duplicate = false;
+    orderNames.forEach((name) => {
+      name === orderName.textContent;
+      duplicate = true;
+    });
+    // 중복 확인
+    if (!duplicate) {
+      orderContainer2.appendChild(div1);
+      div1.setAttribute("id", `${orderName.textContent}`);
+      div1.appendChild(div2);
+      p1.innerHTML = orderName.textContent;
+      div2.appendChild(p1);
+      div2.appendChild(div3);
+      p2.innerHTML = quantity.textContent + "x";
+      div3.appendChild(p2);
+      p3.innerHTML = price.textContent;
+      div3.appendChild(p3);
+    }
   });
 });
